aaeb6b00533df4917ab8a76c9b07d53f7181510b
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-04-09 18:15:54 +0000

    ...

--- js/script.js
+++ js/script.js
@@ -6,9 +6,13 @@
 "use strict"
 
 function findVolumeClicked() {
+  // List the 3 dimensions we will recieve from the user as variables 
+  // Use parseFloat so the user can input a decimal
   const lengthInput = parseFloat(document.getElementById("length-entered").value)
   const widthInput = parseFloat(document.getElementById("width-entered").value)
   const heightInput = parseFloat(document.getElementById("height-entered").value)
+  // Create a variable named "volume" which calculates all of the users inputs
   const volume = (lengthInput * widthInput * heightInput) / 3
+  // Ouput the answer to the user in the class "answer"
   document.getElementById("answer").innerHTML = "The volume is: " + volume.toFixed(2) + " mm<sup>3"
 }
